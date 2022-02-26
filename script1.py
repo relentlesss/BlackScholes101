717504a1b411e2efb0d7b5fa18bbf8990e43d93b
relentlesss <50775594+relentlesss@users.noreply.github.com>
2022-02-27 00:13:08 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -3,7 +3,7 @@ from blackfuncplt import *
 
 def main():
 
-    # define and initialize arguments
+    # Define and initialize arguments
     stck_p = 30
     strike_p = 25
     time_to_mat = 0.1
@@ -11,18 +11,18 @@ def main():
     div_yield = 0.00
     stck_vol = 0.2
    
-    # compute call price
+    # Compute call price
     call_price = call_bs(stck_p, strike_p, time_to_mat, rf_rate, div_yield, stck_vol)
     print(f'Call price: {round(call_price,4)}')
    
-    # compute put price
+    # Compute put price
     put_price = put_bs(stck_p, strike_p, time_to_mat, rf_rate, div_yield, stck_vol)
     print(f'Put price: {round(put_price,4)}')
    
-    # plot call price vs. partial parameter change
+    # Plot call price vs. partial parameter change
     call_des(stck_p, strike_p, time_to_mat, rf_rate, div_yield, stck_vol)
    
-    # plot put price vs. partial parameter change
+    # Plot put price vs. partial parameter change
     put_des(stck_p, strike_p, time_to_mat, rf_rate, div_yield, stck_vol)
 
 
